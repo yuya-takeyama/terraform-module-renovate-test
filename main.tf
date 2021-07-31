061c400e34d95fddd903cf4e8c11213e501c3794
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2021-08-01 02:27:58 +0900

    Initial

---
+++ main.tf
@@ -0,0 +1,25 @@
+module "foo" {
+  source = "git@github.com:yuya-takeyama/terraform-module-foo.git?ref=v0.3.0"
+
+  cnt = 1
+  triggers = {
+    foo = "FOO"
+  }
+}
+
+module "bar" {
+  source = "git@github.com:yuya-takeyama/terraform-module-bar.git?ref=v0.1.0"
+
+  keepers = {
+    bar = "BAR"
+  }
+}
+
+module "baz" {
+  source = "git@github.com:yuya-takeyama/terraform-module-baz.git?ref=v0.2.0"
+
+  cnt = 3
+  triggers = {
+    baz = "BAZ"
+  }
+}
